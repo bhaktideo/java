3f40efd9b714f62de2c3e4156a63491b57581b9a
Bhakti Deo <bhaktideo21@gmail.com>
2025-01-09 13:40:21 +0530

    This is the introduction to debugging tools in IntelliJ Idea

---
+++ src/section_05_introduction_to_java/example1.java
@@ -0,0 +1,10 @@
+package section_05_introduction_to_java;
+
+import java.lang.*;
+
+public class example1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to JAVA programming course");
+    }
+}
---
+++ src/section_05_introduction_to_java/example2.java
@@ -0,0 +1,17 @@
+package section_05_introduction_to_java;
+
+import java.lang.*;
+
+public class example2 {
+    public static void main(String[] args)
+    {
+        System.out.println("First name: Bhakti");
+        System.out.println("Last name: Deo");
+        System.out.println("College name: Yeshwantrao Chavan College of Engineering(YCCE), Nagpur");
+        System.out.println("Branch: Electronics and Telecommunication");
+        System.out.println("CGPA: 9.6");
+        System.out.println("Mobile: 9146946780");
+        System.out.println("Email: bhaktideo21@gmail.com");
+        System.out.println("Languages: C, Python, SQL, Verilog");
+    }
+}
